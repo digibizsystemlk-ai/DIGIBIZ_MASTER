62d2f49879f610b291c8c313b5a617b9006d5805
DIGIBIZ Admin <biz.sirimal@gmail.com>
2026-08-07 06:54:29 +0530

    Update hero heading text to 'අනුරූපී කළමනාකරණ පද්ධතිය'

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "digibiz-retail-v2031";
+const CACHE_NAME = "digibiz-retail-v2032";
 
 // Force immediate activation and take over all clients
 self.addEventListener("install", (event) => {
