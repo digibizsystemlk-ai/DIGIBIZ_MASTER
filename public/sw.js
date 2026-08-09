7eed0eeedd3d72b22c357a2f993bcda106844bd5
DIGIBIZ Admin <biz.sirimal@gmail.com>
2026-08-09 09:18:57 +0530

    Relocate Danger Zone to bottom of page and simplify Super Admin Direct Access button

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "digibiz-retail-v2070";
+const CACHE_NAME = "digibiz-retail-v2071";
 
 // Force immediate activation and take over all clients
 self.addEventListener("install", (event) => {
